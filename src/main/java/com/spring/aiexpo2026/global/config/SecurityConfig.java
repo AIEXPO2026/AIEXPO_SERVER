07b1e587c7e4e3039aacf2669a3d40f6a01c28ea
김경윤 <l982930009@gmail.com>
2026-02-18 22:56:24 +0900

    feat: add swagger

--- src/main/java/com/spring/aiexpo2026/global/config/SecurityConfig.java
+++ src/main/java/com/spring/aiexpo2026/global/config/SecurityConfig.java
@@ -39,7 +39,8 @@ public class SecurityConfig {
 						auth
 								.requestMatchers(
 										"/auth/signup", "/auth/signin",
-										"/auth/email/send", "/auth/email/verify"
+										"/auth/email/send", "/auth/email/verify",
+										"/swagger-ui/**", "/v3/api-docs/**"
 								).permitAll()
 
 								.requestMatchers(
