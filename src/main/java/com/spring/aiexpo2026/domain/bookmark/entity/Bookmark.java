3e53d694270eded6ce9247f08065c5c90f036b05
이도연 <dgsw0509@dgsw.hs.kr>
2026-02-25 20:25:51 +0900

    delete : table annotation

--- src/main/java/com/spring/aiexpo2026/domain/bookmark/entity/Bookmark.java
+++ src/main/java/com/spring/aiexpo2026/domain/bookmark/entity/Bookmark.java
@@ -15,7 +15,6 @@ import java.time.LocalDateTime;
 @SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "bookmark")
 public class Bookmark {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
