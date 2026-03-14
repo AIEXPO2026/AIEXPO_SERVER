46e85b6446478877ef4dfdbc9dd9c11f68831cd1
김경윤 <l982930009@gmail.com>
2026-03-14 22:49:40 +0900

    feat: add retunr travelId

--- src/main/java/com/spring/aiexpo2026/domain/travel/data/response/FinishTravelResponse.java
+++ src/main/java/com/spring/aiexpo2026/domain/travel/data/response/FinishTravelResponse.java
@@ -1,10 +1,12 @@
 package com.spring.aiexpo2026.domain.travel.data.response;
 
 public record FinishTravelResponse(
+		Long id,
 		String message
 ) {
 
-	public static FinishTravelResponse of(String message) {
-		return new FinishTravelResponse(message);
+	public static FinishTravelResponse of(Long id,
+										  String message) {
+		return new FinishTravelResponse(id, message);
 	}
 }
